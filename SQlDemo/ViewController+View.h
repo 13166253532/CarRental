6617bdc8be0cf3295192b55ccefe7f58627027ff
13166253532 <13166253532@163.com>
2017-11-30 14:45:31 +0800

    类的扩展

---
+++ SQlDemo/ViewController+View.h
@@ -0,0 +1,13 @@
+//
+//  ViewController+View.h
+//  SQlDemo
+//
+//  Created by xiejiangbo on 2017/11/30.
+//  Copyright © 2017年 yin chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController (View)
+
+@end
---
+++ SQlDemo/ViewController+View.m
@@ -0,0 +1,13 @@
+//
+//  ViewController+View.m
+//  SQlDemo
+//
+//  Created by xiejiangbo on 2017/11/30.
+//  Copyright © 2017年 yin chen. All rights reserved.
+//
+
+#import "ViewController+View.h"
+
+@implementation ViewController (View)
+
+@end
